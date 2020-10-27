2b7d8f2b0edc5024d43e492accf47155b5c7f755
Omid <omidksand@gmail.com>
2020-10-26 23:20:18 -0400

    Implement default route and IP to geolocation request.

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const cors = require('cors');
+const MaxMind = require('./lib/maxmind');
+
 
 const app = express();
 
 app.use(cors());
 
-
 app.get('/', (req, res) => {
    res.json({
       success: true,
       message: `IP Geolocation Service v0.1`
+   });
+});
+
+app.get('/:ip', (req, res) => {
+   let { ip = null } = req.params,
+      result = ip ? MaxMind.findIpLocation(ip) : null;
+
+   res.json(result || {
+      success: false,
+      message: 'Invalid IP address.'
    });
 });
 
